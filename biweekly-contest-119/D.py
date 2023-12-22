6a99c8bfe4b9410908a468c7a94f677eeb251efa
Rohan Sumant <r.s.sumant@gmail.com>
2023-12-21 20:42:57 -0800

    Removed redundant whitespace

--- biweekly-contest-119/D.py
+++ biweekly-contest-119/D.py
@@ -1,28 +1,28 @@
 class Solution:
     def numberOfSets(self, n: int, maxDistance: int, roads: List[List[int]]) -> int:
-        
+
         roads.sort(key = lambda x: x[2], reverse = True)
         G = {}
         for u,v,cost in roads:
             G[u,v] = G[v,u] = cost
-            
+
         #print(G)
         def ok(a):
-            
+
             N = len(a)
             if N <= 1:
                 return True
-            
+
             MAX = int(1e6)
             dp = [[MAX]*n for _ in range(n)]
-            
+
             for i in a:
                 for j in a:
                     if i == j:
                         dp[i][j] = 0
                     else:
                         dp[i][j] = dp[j][i] = G[i,j] if (i,j) in G else MAX
-                
+
             # floyd warshall's
             for i in range(N):
                 for j in range(N):
@@ -36,7 +36,7 @@ class Solution:
                         ans = False
             #print(a, dp, ans)
             return ans
-        
+
         ans = 0
         for mask in range(1<<n):
             ls = []
@@ -45,5 +45,5 @@ class Solution:
                     ls.append(j)
             if ok(ls):
                 ans += 1
-                
+
         return ans
